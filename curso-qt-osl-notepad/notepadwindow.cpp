34676daa17da595f2bc9dc683e462a520e049b77
Darias <dariasteam94@gmail.com>
2015-02-14 14:06:59 +0000

    Primer commit

--- curso-qt-osl-notepad/notepadwindow.cpp
+++ curso-qt-osl-notepad/notepadwindow.cpp
@@ -7,21 +7,40 @@ NotepadWindow::NotepadWindow(QWidget *parent)
     this->setGeometry(30, 30, 800, 600);
 
     //Establecemos el título de la ventana
-    this->setWindowTitle(tr("Super editor de texto"));
-
+    this->setWindowTitle(tr("Súper editor de texto"));  //función tr para poder realizar traducciones a posteriori
     //Inicializamos los menús
     mainMenu_ = new QMenuBar(this);
+    barra_ = new QToolBar(this);
 
-    mnuArchivo_ = new QMenu(tr("&Archivo"), this);
+    mnuArchivo_ = new QMenu(tr("&Archivo"), this);      //Título y padre
     mainMenu_->addMenu(mnuArchivo_);
 
     actArchivoAbrir_ = new QAction(tr("&Abrir"), this);
     actArchivoAbrir_->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_O));
     mnuArchivo_->addAction(actArchivoAbrir_);
+    barra_->addAction(actArchivoAbrir_);
 
     actArchivoGuardar_ = new QAction(tr("&Guardar"), this);
     actArchivoGuardar_->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_S));
     mnuArchivo_->addAction(actArchivoGuardar_);
+    barra_->addAction(actArchivoGuardar_);
+
+    actArchivoSalir_ = new QAction(tr("&Salir"), this);
+    actArchivoSalir_->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_Q));
+    mnuArchivo_->addAction(actArchivoSalir_);
+    barra_->addAction(actArchivoSalir_);
+
+    negrita_ = new QAction(tr("Negrita"), this);
+    cursiva_ = new QAction(tr("Cursiva"), this);
+    subrayado_ = new QAction(tr("Subrayado"), this);
+    color_ = new QAction(tr("Color"), this);
+    negrita_->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_B));
+    cursiva_->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_I));
+    subrayado_->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_U));
+    barra_->addAction(negrita_);
+    barra_->addAction(cursiva_);
+    barra_->addAction(subrayado_);
+    barra_->addAction(color_);
 
     mnuEditar_ = new QMenu(tr("&Editar"), this);
     mainMenu_->addMenu(mnuEditar_);
@@ -34,38 +53,72 @@ NotepadWindow::NotepadWindow(QWidget *parent)
     actEditarPegar_->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_V));
     mnuEditar_->addAction(actEditarPegar_);
 
+    actEditarCortar_ = new QAction(tr("&Cortar"), this);
+    actEditarCortar_->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_X));
+    mnuEditar_->addAction(actEditarCortar_);
+
+    actEditarDeshacer_ = new QAction(tr("&Deshacer"), this);
+    actEditarDeshacer_->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_Z));
+    mnuEditar_->addAction(actEditarDeshacer_);
+
+    actEditarRehacer_ = new QAction(tr("&Rehacer"), this);
+    actEditarRehacer_->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_Y));
+    mnuEditar_->addAction(actEditarRehacer_);
+
     mnuFormato_ = new QMenu(tr("&Formato"), this);
     mainMenu_->addMenu(mnuFormato_);
+
+    mnuAyuda_ = new QMenu(tr("&Ayuda"), this);
+    mainMenu_->addMenu(mnuAyuda_);
+
+    AcercaDe_ = new QAction(tr("&Acerca de"), this);
+    mnuAyuda_->addAction(AcercaDe_);
 
     actFormatoFuente_ = new QAction(tr("&Fuente"), this);
     mnuFormato_->addAction(actFormatoFuente_);
 
     //Agregamos la barra de menú a la ventana
-    this->setMenuBar(mainMenu_);
+    this->setMenuBar(mainMenu_);    //this innecesario en este caso
+    addToolBar(barra_);
 
     //Inicializamos el editor de texto
-    txtEditor_ = new QPlainTextEdit(this);
+    txtEditor_ = new QTextEdit(this);
 
     //Conectamos las acciones de los menús con nuestros slots
+    //Conectar acción-señal-conquién-conquéslot
     connect(actArchivoAbrir_,   SIGNAL(triggered()), this,          SLOT(alAbrir()));
     connect(actArchivoGuardar_, SIGNAL(triggered()), this,          SLOT(alGuardar()));
-    connect(actEditarCopiar_,   SIGNAL(triggered()), txtEditor_,    SLOT(copy()));
+    connect(actArchivoSalir_, SIGNAL(triggered()), this,          SLOT(alSalir()));
+    connect(AcercaDe_, SIGNAL(triggered()), this,          SLOT(acercade()));
+    connect(actEditarCopiar_,   SIGNAL(triggered()), txtEditor_,    SLOT(copy()));          //esta conecta con un objeto de qt
+    connect(actEditarCortar_,   SIGNAL(triggered()), txtEditor_,    SLOT(cut()));
     connect(actEditarPegar_,    SIGNAL(triggered()), txtEditor_,    SLOT(paste()));
+    connect(actEditarDeshacer_,    SIGNAL(triggered()), txtEditor_,    SLOT(undo()));
+    connect(actEditarRehacer_,    SIGNAL(triggered()), txtEditor_,    SLOT(redo()));
     connect(actFormatoFuente_,  SIGNAL(triggered()), this,          SLOT(alFuente()));
 
+    connect(negrita_,  SIGNAL(triggered()), this,      SLOT(setBold()));
+    connect(subrayado_,  SIGNAL(triggered()), this,      SLOT(setUnderline()));
+    connect(cursiva_,  SIGNAL(triggered()), this,      SLOT(setItalic()));
+    connect(color_,  SIGNAL(triggered()), this,      SLOT(cambiarColor()));
+
+
     //Agregamos el editor de texto a la ventana
-    this->setCentralWidget(txtEditor_);
+    setCentralWidget(txtEditor_);
 }
 
 NotepadWindow::~NotepadWindow()
 {
-    //Liberamos los recursos
+    //Liberamos los recursos con el propio recolector de Qt, en este caso innecesario ya que todos tienen padre
     mainMenu_->deleteLater();
     actArchivoAbrir_->deleteLater();
     actArchivoGuardar_->deleteLater();
+    actArchivoSalir_ ->deleteLater();
     mnuArchivo_->deleteLater();
     actEditarCopiar_->deleteLater();
     actEditarPegar_->deleteLater();
+    actEditarCortar_->deleteLater();
+    actEditarDeshacer_->deleteLater();
     mnuEditar_->deleteLater();
     actFormatoFuente_->deleteLater();
     mnuFormato_->deleteLater();
@@ -86,7 +139,7 @@ void NotepadWindow::alAbrir()
         archivo.setFileName(nombreArchivo);
         if (archivo.open(QFile::ReadOnly)) {
             //Si se pudo abrir el archivo, lo leemos y colocamos su contenido en nuestro editor
-            txtEditor_->setPlainText(archivo.readAll());
+            txtEditor_->setPlainText(archivo.readAll());    //No hace falta especificar la conversión de qbytearray a qstring porque ya está decarado un constructor que lo sabe hacer e
             //Se cierra el fichero
             archivo.close();
         }
@@ -104,7 +157,10 @@ void NotepadWindow::alGuardar()
     if (nombreArchivo != "") {
         //Intentamos abrir el archivo
         QFile archivo;
-        archivo.setFileName(nombreArchivo + ".txt");
+        if(!nombreArchivo.endsWith(".txt")){
+            nombreArchivo += ".txt";
+        }
+        archivo.setFileName(nombreArchivo);
         if (archivo.open(QFile::WriteOnly | QFile::Truncate)) {
             //Si se pudo abrir el archivo, escribimos el contenido del editor
             archivo.write(txtEditor_->toPlainText().toUtf8());
@@ -120,6 +176,37 @@ void NotepadWindow::alFuente()
     QFont font = QFontDialog::getFont(&ok, txtEditor_->font(), this);
     if (ok) {
         // Si el usuario hizo click en OK, se establece la fuente seleccionada
-        txtEditor_->setFont(font);
+        //txtEditor_->setFont(font);
+        txtEditor_->setCurrentFont(font);
     }
+}
+
+void NotepadWindow::setBold(){
+    if(txtEditor_->fontWeight()==86){
+        txtEditor_->setFontWeight(50);
+    }else{
+        txtEditor_->setFontWeight(86);
+    }
+}
+
+void NotepadWindow::setUnderline(){
+  txtEditor_->setFontUnderline(!txtEditor_->fontUnderline());
+}
+
+void NotepadWindow::setItalic(){
+  txtEditor_->setFontItalic(!txtEditor_->fontItalic());
+}
+
+void NotepadWindow::cambiarColor(){
+
+}
+
+void NotepadWindow::alSalir(){
+    close();
+}
+
+void NotepadWindow::acercade(){
+    QMessageBox Ayuda;
+    Ayuda.setText("Esta es la ayuda");
+    Ayuda.exec();
 }
--- curso-qt-osl-notepad/notepadwindow.h
+++ curso-qt-osl-notepad/notepadwindow.h
@@ -4,40 +4,61 @@
 #include <QMainWindow>
 //Incluimos librerias necesarias
 #include <QMenuBar>
+#include <QMessageBox>
 #include <QMenu>
 #include <QAction>
-#include <QPlainTextEdit>
-#include <QFileDialog>
-#include <QFile>
-#include <QFontDialog>
-#include <QClipboard>
-#include <QKeySequence>
+#include <QPlainTextEdit>       //Cuadro de texto plano
+#include <QFileDialog>          //VEntana de Abrir-guardar
+#include <QFile>                //Clase que permite abrir ficheros
+#include <QFontDialog>          //Cuadro de selección de fuente
+#include <QClipboard>           //Portapepeles
+#include <QKeySequence>         //Accesos directos
+#include <QToolBar>
+#include <QColor>
 
 class NotepadWindow : public QMainWindow
 {
-    Q_OBJECT
+    Q_OBJECT        //Directiva al precompilador de Qt para avisar que hay palabras reservadas de Qt
 
 public:
-    NotepadWindow(QWidget *parent = 0);
+    NotepadWindow(QWidget *parent = 0); //Tiene padre?
     ~NotepadWindow();
 
 private slots:
     void alAbrir();
     void alGuardar();
     void alFuente();
+    void alSalir();
+    void acercade();
+    void setBold();
+    void setUnderline();
+    void setItalic();
+    void cambiarColor();
 
 private:
     QMenuBar*       mainMenu_;
     QMenu*          mnuArchivo_;
+    QMenu*          mnuAyuda_;
     QAction*        actArchivoAbrir_;
+    QAction*        AcercaDe_;
     QAction*        actArchivoGuardar_;
+    QAction*        actArchivoSalir_;
     QMenu*          mnuFormato_;
     QAction*        actFormatoFuente_;
     QMenu*          mnuEditar_;
     QAction*        actEditarCopiar_;
     QAction*        actEditarPegar_;
-    QPlainTextEdit* txtEditor_;
-    QClipboard *    portapapeles_;
+    QAction*        actEditarCortar_;
+    QAction*        actEditarDeshacer_;
+    QAction*        actEditarRehacer_;
+    QTextEdit*      txtEditor_;
+    QClipboard*     portapapeles_;
+    QToolBar*       barra_;
+    QAction*        negrita_;
+    QAction*        cursiva_;
+    QAction*        subrayado_;
+    QAction*        color_;
+
 
 };
 
